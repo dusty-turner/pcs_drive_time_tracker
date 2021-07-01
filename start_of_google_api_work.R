c2cf396373995aa7b73da9e98277ea01cca82a55
Dusty Turner <dusty.s.turner@gmail.com>
2021-07-01 14:44:53 +0000

    set up cron and also to publish

--- start_of_google_api_work.R
+++ start_of_google_api_work.R
@@ -1,19 +1,37 @@
 library(tidyverse)
 library(gmapsdistance)
+source("/home/ubuntu/Projects/pcs_drive_time_tracker/source_files/drive_time_source.R")
 
-set.api.key(Sys.getenv("key"))
+set.api.key(key = key)
 
-origin <- str_replace_all(Sys.getenv("start1")," ", "+")
-destination <- str_replace_all(Sys.getenv("end1")," ", "+")
+map_once <- function(start = start1, end = end1){
+
+origin <- str_replace_all(start," ", "+")
+destination <- str_replace_all(end," ", "+")
 
 results <- 
-  gmapsdistance(origin = origin, destination = destination,
-              mode = "driving", shape = "wide")
+  gmapsdistance(origin = origin, destination = destination,mode = "driving", shape = "wide")
 
+new_results <-
 tibble(origin = str_replace_all(origin,"\\+"," "), 
        destination = str_replace_all(destination, "\\+"," "), 
        time = results$Time/60, 
        distance = results$Distance/1609.34,
        search_time = lubridate::now()
        )
+
+return(new_results)
+
+}
+
+new_results <-
+map2_dfr(.x = list_start, .y = list_end, .f = ~map_once(start = .x,end = .y))
+
+old_results <- read_csv("/home/ubuntu/Projects/pcs_drive_time_tracker/results/results.csv")
+
+old_results %>% 
+  bind_rows(new_results) %>% 
+  distinct() %>% 
+  write_csv("/home/ubuntu/Projects/pcs_drive_time_tracker/results/results.csv")
+
 
